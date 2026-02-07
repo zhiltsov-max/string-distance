2573801290dab6126c1200fdfb6f981ab2a43437
Maxim Zhiltsov <zhiltsov.max35@gmail.com>
2026-02-07 22:12:26 +0300

    Add performance tests and metrics (#2)

--- README.md
+++ README.md
@@ -50,3 +50,30 @@ levenshtein_utf(a: &String, b: &String) -> i32
 ```bash
 cargo test
 ```
+
+## Performance
+
+**levenshtein()**:
+
+| Charset | Length | Time (µs) |
+| --- | --- | --- |
+| ASCII | 16 | 0.730 ±0.003 |
+| ASCII | 64 | 7.540 ±0.03 |
+| ASCII | 128 | 26.730 ±0.1 |
+| ASCII | 256 | 98.987 ±0.3 |
+| ASCII | 512 | 383.92 ±0.6 |
+| ASCII | 1000 | 1414.7 ±3 |
+| UTF8 | 16 | 2.007 ±0.01 |
+| UTF8 | 64 | 21.510 ±0.1 |
+| UTF8 | 128 | 80.166 ±0.2 |
+| UTF8 | 256 | 305.23 ±2 |
+| UTF8 | 512 | 1650.7 ±100 |
+| UTF8 | 1000 | 31150 ±300 |
+
+* CPU: AMD Ryzen 7 6800H
+
+## Benchmarking
+
+```bash
+cargo bench
+```
---
+++ benches/my_benchmark.rs
@@ -0,0 +1,83 @@
+use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
+use rand;
+use std::hint::black_box;
+use string_distance;
+
+fn generate_random_string<R: rand::Rng, D: rand::distr::Distribution<char>>(
+    length: usize,
+    rng: &mut R,
+    distribution: &D,
+) -> String {
+    let mut s = String::with_capacity(length * 4);
+    for _ in 0..length {
+        s.push(rng.sample(distribution));
+    }
+    return s;
+}
+
+struct AlphanumericChar {
+    alnum: rand::distr::Alphanumeric,
+}
+
+impl rand::distr::Distribution<char> for AlphanumericChar {
+    fn sample<R: rand::Rng + ?std::marker::Sized>(&self, rng: &mut R) -> char {
+        return char::from(self.alnum.sample(rng));
+    }
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let lengths = [16, 64, 128, 256, 512, 1000];
+
+    let ascii_chars = AlphanumericChar {
+        alnum: rand::distr::Alphanumeric,
+    };
+    let utf8_chars = rand::distr::StandardUniform;
+    let mut rng = rand::rng();
+
+    for length in lengths {
+        c.bench_function(
+            &format!(
+                "levenshtein for {charset} strings of length {length}",
+                charset = "ASCII",
+                length = length
+            ),
+            |b| {
+                b.iter_batched(
+                    || {
+                        return (
+                            generate_random_string(length, &mut rng, &ascii_chars),
+                            generate_random_string(length, &mut rng, &ascii_chars),
+                        );
+                    },
+                    |v| string_distance::levenshtein(black_box(&v.0), black_box(&v.1)),
+                    BatchSize::SmallInput,
+                )
+            },
+        );
+    }
+
+    for length in lengths {
+        c.bench_function(
+            &format!(
+                "levenshtein for {charset} strings of length {length}",
+                charset = "UTF8",
+                length = length
+            ),
+            |b| {
+                b.iter_batched(
+                    || {
+                        return (
+                            generate_random_string(length, &mut rng, &utf8_chars),
+                            generate_random_string(length, &mut rng, &utf8_chars),
+                        );
+                    },
+                    |v| string_distance::levenshtein(black_box(&v.0), black_box(&v.1)),
+                    BatchSize::SmallInput,
+                )
+            },
+        );
+    }
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
